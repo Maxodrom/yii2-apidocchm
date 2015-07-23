2f13477211aadea6baf6576c8faaaa91bc00b125
bisubus <bisubus@users.noreply.github.com>
2015-07-24 00:08:57 +0300

    windows batch extensions paths

--- generate-chm.bat
+++ generate-chm.bat
@@ -1,9 +1,13 @@
 @echo off
 set HTMLHELP_PATH=..\_htmlhelp
 
+if exist _Docs rd _Docs /s /q
+
 echo.
 echo API
-php apidoc api --overwrite --template=bootstrapchm vendor/yiisoft/yii2-dev ./_Docs
+php apidoc api --overwrite --template=bootstrapchm vendor/yiisoft/yii2-dev,vendor/yiisoft/yii2-apidoc,vendor/yiisoft/yii2-authclient,vendor/yiisoft/yii2-bootstrap,vendor/yiisoft/yii2-codeception,vendor/yiisoft/yi
+i2-composer,vendor/yiisoft/yii2-debug,vendor/yiisoft/yii2-elasticsearch,vendor/yiisoft/yii2-faker,vendor/yiisoft/yii2-gii,vendor/yiisoft/yii2-imagine,vendor/yiisoft/yii2-jui,vendor/yiisoft/yii2-mongodb,vendor/yii
+soft/yii2-redis,vendor/yiisoft/yii2-smarty,vendor/yiisoft/yii2-sphinx,vendor/yiisoft/yii2-swiftmailer,vendor/yiisoft/yii2-twig ./_Docs
 
 echo.
 echo Guide
